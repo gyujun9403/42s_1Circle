2d4609ded4963e1980fc4cc8169834a4dd6c1375
gyujun9403 <gujun112@naver.com>
2021-05-27 02:40:51 +0900

    ver2.0

---
+++ get-next-line/get_next_line_util.c
@@ -0,0 +1,90 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_util.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ygj <ygj@student.42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/24 18:07:21 by ygj               #+#    #+#             */
+/*   Updated: 2021/05/26 16:21:03 by ygj              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+t_bufflst   *find_lastlst(t_bufflst *lst)
+{
+    while (lst->next != NULL)
+        lst = lst->next;
+    return (lst);
+}
+
+t_bufflst   *add_lastlst(t_bufflst **lst)
+{
+    t_bufflst   *last;
+
+    if (*lst == NULL)
+    {
+        *lst = (t_bufflst *)malloc(sizeof(t_bufflst));
+        last = *lst;
+    }
+    else
+    {
+        last = find_lastlst(*lst);
+        last->next = (t_bufflst *)malloc(sizeof(t_bufflst));
+        last = last->next;
+    }
+    last->is_eof = 0;
+    last->num_nl = 0;
+    last->next = NULL;
+    last->buff = (char *)malloc(BUFFER_SIZE * sizeof(char) + 1);
+    if (last->buff == NULL)
+        last->state = -1;
+    else
+        last->state = 0;
+    last->st_buff = last->buff;
+    return (last);
+}
+
+t_bufflst   *free_lst(t_bufflst **lst, short flg)
+{
+    t_bufflst   *temp;
+
+    if(flg == 0)
+    {
+        temp = (*lst)->next;
+        free((*lst)->buff);
+        free(*lst);
+        *lst = temp;
+    }
+    else
+        while (*lst != NULL)
+        {
+            temp = (*lst)->next;
+            free((*lst)->buff);
+            free(*lst);
+            *lst = temp;
+        }
+    return (temp);
+}
+
+size_t    gnl_strchr(const char *s, int c)
+{
+        size_t  i;
+        char    *answer;
+
+        i = 0;
+        answer = NULL;
+        while (*(s + i) != '\0')
+        {
+                if (*(s + i) == (unsigned char)c)
+                {
+                        answer = (char *)s + i;
+                        break ;
+                }
+                i++;
+        }
+        if (*(s + i) == '\0' && (unsigned char)c == '\0')
+                answer = (char *)s + i;
+        return (i);
+}
